66b4456bc977ebc1b36f84738abdff58f685bb41
Mariya Podosinova <mariya.podosinova@gmail.com>
2024-12-03 10:57:00 +0000

    building a screenn component and wrapping react around redux

--- src/components/InboxScreen.jsx
+++ src/components/InboxScreen.jsx
@@ -1,16 +1,11 @@
 import { useEffect } from "react";
-
 import { useDispatch, useSelector } from "react-redux";
-
 import { fetchTasks } from "../lib/store";
-
 import TaskList from "./TaskList";
 
 export default function InboxScreen() {
   const dispatch = useDispatch();
-  // We're retrieving the error field from our updated store
   const { error } = useSelector((state) => state.taskbox);
-  // The useEffect triggers the data fetching when the component is mounted
   useEffect(() => {
     dispatch(fetchTasks());
   }, []);
